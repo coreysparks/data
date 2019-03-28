dd1faba32ebd69072a29702574ec5b89c1b74990
Corey Sparks <corey.sparks@utsa.edu>
2019-03-28 12:33:38 -0500

    IPUMS data for SOC 579
    IPUMS USA 2016  1 year file

--- usa_00074.R
+++
@@ -1,7 +0,0 @@
-# NOTE: To load data, you must download both the extract's data and the DDI
-# and also set the working directory to the folder with these files (or change the path below).
-
-if (!require("ipumsr")) stop("Reading IPUMS data into R requires the ipumsr package. It can be installed using the following command: install.packages('ipumsr')")
-
-ddi <- read_ipums_ddi("usa_00074.xml")
-data <- read_ipums_micro(ddi)
